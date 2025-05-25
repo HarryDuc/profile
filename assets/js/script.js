5b4453193fd4f3d7178e5c4f6d2677b50bc2ef83
Vũ Minh Đức <vuminhduc.contact@gmail.com>
2025-05-25 18:32:39 +0700

    Update Avatar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,44 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
 window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('active');
-}
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
 
 document.addEventListener("DOMContentLoaded", function () {
-    var submitted = false;
-    var form = document.getElementById("ss-form");
-    var hiddenIframe = document.getElementById("hidden_iframe");
+  var submitted = false;
+  var form = document.getElementById("ss-form");
+  var hiddenIframe = document.getElementById("hidden_iframe");
 
-    form.onsubmit = function () {
-        submitted = true;
-        alert('Bạn đã gửi thông tin thành công tới hệ thống!');
-    };
+  form.onsubmit = function () {
+    submitted = true;
+    alert("Bạn đã gửi thông tin thành công tới hệ thống!");
+  };
 
-    hiddenIframe.onload = function () {
-        if (submitted) {
-            console.log('Send Successful to system.');
-            form.reset();
-        }
-    };
+  hiddenIframe.onload = function () {
+    if (submitted) {
+      console.log("Send Successful to system.");
+      form.reset();
+    }
+  };
 });
 
-const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Backend Developer', 'Blockchain Developer', 'Web Designer', 'Photographer'],
-    typeSpeed: 80,
-    backSpeed: 80,
-    backDelay: 1200,
-    loop: true,
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "Frontend Developer",
+    "Backend Developer",
+    "Blockchain Developer",
+    "Web Designer",
+    "Photographer",
+  ],
+  typeSpeed: 80,
+  backSpeed: 80,
+  backDelay: 1200,
+  loop: true,
 });
